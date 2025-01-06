a6065245f3af3668e4eccf09e14932ea58262221
DeimerRoncancio <deimerroncancio@gmail.com>
2025-01-05 21:45:52 -0500

    Jar creado y application.properties modificado

--- target/generated-sources/annotations/com/majestic/food/api/majestic_food_api/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/majestic/food/api/majestic_food_api/mappers/UserMapperImpl.java
@@ -10,8 +10,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-05T21:22:41-0500",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.41.0.v20241217-1506, environment: Java 17.0.13 (Eclipse Adoptium)"
+    date = "2025-01-05T21:44:51-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
 )
 public class UserMapperImpl implements UserMapper {
 
